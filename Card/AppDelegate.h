313c609aeecbe7ec1dfda8beb18b8d556c1311c1
chentou <tongcc8@gmail.com>
2014-07-08 14:46:55 +0800

    Initial Commit

---
+++ Card/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Card
+//
+//  Created by Chentou TONG on 8/7/14.
+//  Copyright (c) 2014 UOCEAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Card/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Card
+//
+//  Created by Chentou TONG on 8/7/14.
+//  Copyright (c) 2014 UOCEAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
